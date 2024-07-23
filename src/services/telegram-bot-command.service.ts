b611e597da3e91a925f0cd0c878b130c111f5509
Ricardo Mantilla <mantillasanchezr@gmail.com>
2024-07-23 13:50:39 -0500

    Webhook para enviar id del grupo al creador cuando se añada el bot al grupo

--- src/services/telegram-bot-command.service.ts
+++ src/services/telegram-bot-command.service.ts
@@ -1,4 +1,12 @@
 import { Context, Telegraf } from "telegraf";
+import {
+  ChatFromGetChat,
+  ChatMemberAdministrator,
+  ChatMemberOwner,
+  User,
+} from "telegraf/typings/core/types/typegram";
+import telegramServices from "./telegram_services";
+import backLogsServices from "./back-logs-service";
 
 /**
  * Comandos con el bot de telegram
@@ -30,9 +38,122 @@ export function setupCommands(bot: Telegraf): void {
     ctx.reply(`Tu ID de usuario de Telegram es: ${userId}`);
   }
 
+  /**
+   * Manejar el evento cuando el bot sea añadido a un grupo
+   *
+   * @param {*} ctx
+   * @return {*}  {Promise<void>}
+   */
+  async function newChatMembers(ctx: any): Promise<void> {
+    console.log("🚀 ~ newChatMembers ~ Inicia");
+
+    const newMembers: User[] = ctx.message?.new_chat_members;
+    const groupId: number = ctx.chat?.id;
+    let botId: number = NaN;
+
+    try {
+      botId = (await bot.telegram.getMe()).id;
+    } catch (error) {
+      backLogsServices.catchProcessError(
+        `Error: ${error}`,
+        `telegram-bot-commands.service ~ newChatMembers ~ JSON.stringify(error): ${JSON.stringify(
+          error
+        )}`
+      );
+    }
+
+    if (!groupId) {
+      console.log(
+        "🚀 ~ newChatMembers ~ groupId: No se ha obtenido el id del grupo"
+      );
+      ctx.reply(`Ha ocurrido un error obteniendo el id del grupo`);
+      return;
+    }
+    console.log(`🚀 ~ newChatMembers ~ groupId: ${groupId}`);
+
+    // Datos del grupo
+    let groupData: ChatFromGetChat = null as any;
+
+    try {
+      groupData = await bot.telegram.getChat(groupId);
+    } catch (error) {
+      backLogsServices.catchProcessError(
+        `Error: ${error}`,
+        `telegram-bot-commands.service ~ newChatMembers ~ JSON.stringify(error): ${JSON.stringify(
+          error
+        )}`
+      );
+    }
+
+    // Verificar si el bot es el nuevo miembro añadido
+    const botAdded: boolean = newMembers.some(
+      (member: User) => member.id === botId && member.is_bot
+    );
+
+    if (!botAdded) {
+      console.log(
+        "🚀 ~ newChatMembers ~ botAdded: No se ha añadido el bot al grupo"
+      );
+      ctx.reply(`Ha ocurrido un error añadiendo el bot al grupo`);
+      return;
+    }
+
+    // Buscar un creador o administrador del grupo
+    let admins: (ChatMemberOwner | ChatMemberAdministrator)[] = null as any;
+
+    try {
+      admins = await bot.telegram.getChatAdministrators(groupId);
+    } catch (error) {
+      backLogsServices.catchProcessError(
+        `Error: ${error}`,
+        `telegram-bot-commands.service ~ newChatMembers ~ JSON.stringify(error): ${JSON.stringify(
+          error
+        )}`
+      );
+    }
+
+    let adminUser: ChatMemberOwner | ChatMemberAdministrator =
+      admins.find(
+        (admin: ChatMemberOwner | ChatMemberAdministrator) =>
+          !admin.user.is_bot &&
+          !admin.is_anonymous &&
+          admin.status === "creator"
+      ) ||
+      admins.find(
+        (admin: ChatMemberOwner | ChatMemberAdministrator) =>
+          !admin.user.is_bot &&
+          !admin.is_anonymous &&
+          admin.status === "administrator"
+      ) ||
+      (null as any);
+
+    if (!adminUser) {
+      console.log(
+        "🚀 ~ newChatMembers ~ adminUser: No se ha encontrado un administrador del grupo"
+      );
+      ctx.reply(`Ha ocurrido un error encontrando al administrador del grupo`);
+      return;
+    }
+
+    // Enviar el id del grupo al dueño del grupo
+    await telegramServices.enviarMensajeBotAUsuario(
+      adminUser.user.id,
+      `Conexion exitosa con el grupo: '${
+        (groupData as any).title
+      }'.\n\nEl Id de tu grupo es: ${groupId}.\n\nNo olvides añadirme como administrador del grupo y darme permisos para añadir usuarios.`
+    );
+
+    return;
+  }
+
   // Comando /my_id
   bot.command("my_id", (ctx: Context) => {
     miId(ctx);
+  });
+
+  // Manejar el evento cuando el bot sea añadido a un grupo
+  bot.on("new_chat_members", async (ctx: any) => {
+    newChatMembers(ctx);
   });
 
   // Comando start
